3f62d9a44e28910e8220324bd77026a23f782f29
Kombi-D <156035718+Kombi-D@users.noreply.github.com>
2024-12-02 15:10:02 -0800

    portin updates

--- LCD/Porting/Porting.ino
+++ LCD/Porting/Porting.ino
@@ -71,6 +71,73 @@
 #include <ui.h>
 #include "lvgl_port_v8.h"
 
+// SimpleTime Example
+#include <WiFi.h>
+#include "time.h"
+#include "esp_sntp.h"
+
+const char *ssid = "VWLife";
+const char *password = "Wolfman1!";
+
+const char *ntpServer1 = "pool.ntp.org";
+const char *ntpServer2 = "time.nist.gov";
+const long gmtOffset_sec = 3600;
+const int daylightOffset_sec = 3600;
+const char *time_zone = "PST8PDT,M3.2.0,M11.1.0";  // TimeZone rule for Europe/Rome including daylight adjustment rules (optional)
+
+char currentTimeString[9] = {};
+const int asciiOffset = 48;
+void printLocalTime() {
+  struct tm timeinfo;
+  if (!getLocalTime(&timeinfo)) {
+    Serial.println("No time available (yet)");
+    return;
+  }
+
+
+  if(9 < timeinfo.tm_hour && 13 > timeinfo.tm_hour || 21 < timeinfo.tm_hour && 24 > timeinfo.tm_hour){
+    currentTimeString[0] = '1';
+  }
+  else {
+    currentTimeString[0] = ' ';
+  }
+  if(13 > timeinfo.tm_hour){
+      currentTimeString[1] = (timeinfo.tm_hour % 10) + asciiOffset;
+  }
+  else{
+      currentTimeString[1] = ((timeinfo.tm_hour - 12) % 10) + asciiOffset;
+  }
+  currentTimeString[2] = ':';
+  currentTimeString[3] = (timeinfo.tm_min / 10) + asciiOffset;
+  currentTimeString[4] = (timeinfo.tm_min % 10) + asciiOffset;
+  currentTimeString[5] = ' ';
+  if(timeinfo.tm)
+  currentTimeString[6] = 
+  currentTimeString[9] = '\0';
+  lv_label_set_text(ui_clock, currentTimeString);
+}// ascii: 0 = 48, 9 = 57;
+
+/*
+struct tm
+{
+  int	tm_sec;
+  int	tm_min;
+  int	tm_hour;
+  int	tm_mday;
+  int	tm_mon;
+  int	tm_year;
+  int	tm_wday;
+  int	tm_yday;
+  int	tm_isdst;
+*/
+
+// Callback function (gets called when time adjusts via NTP)
+void timeavailable(struct timeval *t) {
+  Serial.println("Got time adjustment from NTP!");
+  printLocalTime();
+}
+// END: SimpleTime Example
+
 //Declare custom functions
 void switchScreen();
 void changeUpState();
@@ -160,7 +227,25 @@ volatile byte containerVisible = false;
 lv_obj_t* root_widget = lv_obj_get_parent(lv_scr_act());
 
 void setup(){
-    Serial.begin(115200);    
+  Serial.begin(115200);
+// SimpleTime begin
+  Serial.printf("Connecting to %s ", ssid);
+  WiFi.begin(ssid, password);
+  esp_sntp_servermode_dhcp(1);
+  int WifiAttemptCount = 0;
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println(" CONNECTED");
+
+  // set notification call-back function
+  sntp_set_time_sync_notification_cb(timeavailable);
+  // #define TZ_America_Los_Angeles	PSTR("PST8PDT,M3.2.0,M11.1.0")
+  configTzTime(PSTR("PST8PDT,M3.2.0,M11.1.0"), ntpServer1, ntpServer2);    
+// Simple time end    
+
+
     Serial.println("Squareline porting example start");
 
     // Configure dpad buttons
