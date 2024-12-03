d8778281fe99a78f618c193972687b5a12719644
g1mendoza <g1mendoza@ucsd.edu>
2024-12-02 16:10:29 -0800

    hidden containers

--- LCD/Porting/screens/ui_Home_Page.c
+++ LCD/Porting/screens/ui_Home_Page.c
@@ -107,7 +107,7 @@ lv_obj_set_x( ui_Container1, 1 );
 lv_obj_set_y( ui_Container1, 1 );
 lv_obj_set_align( ui_Container1, LV_ALIGN_CENTER );
 lv_obj_add_state( ui_Container1, LV_STATE_DISABLED );     /// States
-lv_obj_add_flag( ui_Container1, LV_OBJ_FLAG_CHECKABLE | LV_OBJ_FLAG_EVENT_BUBBLE );   /// Flags
+lv_obj_add_flag( ui_Container1, LV_OBJ_FLAG_HIDDEN | LV_OBJ_FLAG_CHECKABLE | LV_OBJ_FLAG_EVENT_BUBBLE );   /// Flags
 lv_obj_clear_flag( ui_Container1, LV_OBJ_FLAG_SCROLLABLE );    /// Flags
 lv_obj_set_style_bg_color(ui_Container1, lv_color_hex(0x003049), LV_PART_MAIN | LV_STATE_DEFAULT );
 lv_obj_set_style_bg_opa(ui_Container1, 255, LV_PART_MAIN| LV_STATE_DEFAULT);
--- LCD/Porting/screens/ui_Task_Page.c
+++ LCD/Porting/screens/ui_Task_Page.c
@@ -127,7 +127,7 @@ lv_obj_set_x( ui_notification, 1 );
 lv_obj_set_y( ui_notification, 1 );
 lv_obj_set_align( ui_notification, LV_ALIGN_CENTER );
 lv_obj_add_state( ui_notification, LV_STATE_DISABLED );     /// States
-lv_obj_add_flag( ui_notification, LV_OBJ_FLAG_CHECKABLE | LV_OBJ_FLAG_EVENT_BUBBLE );   /// Flags
+lv_obj_add_flag( ui_notification, LV_OBJ_FLAG_HIDDEN | LV_OBJ_FLAG_CHECKABLE | LV_OBJ_FLAG_EVENT_BUBBLE );   /// Flags
 lv_obj_clear_flag( ui_notification, LV_OBJ_FLAG_SCROLLABLE );    /// Flags
 lv_obj_set_style_bg_color(ui_notification, lv_color_hex(0x003049), LV_PART_MAIN | LV_STATE_DEFAULT );
 lv_obj_set_style_bg_opa(ui_notification, 255, LV_PART_MAIN| LV_STATE_DEFAULT);
